628220ca8783fd8504d6788a56c3beb4961de86f
krv1974 <63528758+krv1974@users.noreply.github.com>
2024-01-07 15:00:11 +1300

    commit baby

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+23 letters
+1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n
+*/
+import Texts from './imports.json' assert {type: 'json'};
+const texts = Texts;
+
+const letterMap = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"]
+const scriptMap = ["lat", "cyr"]
+
+
+import letters from './letter.json' assert {type: 'json'};
+const letter = letters;
+
+console.log(letter)
+const body = document.body;
